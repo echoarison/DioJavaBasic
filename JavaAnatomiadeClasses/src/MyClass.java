62e1f3d9631159fc17eca16a318bafcdee5b9ac2
Alison <alisson_gamer2000@outlook.com>
2024-07-08 11:40:47 -0300

    Create Packges of files

--- JavaAnatomiadeClasses/src/MyClass.java
+++ JavaAnatomiadeClasses/src/MyClass.java
@@ -1,3 +1,4 @@
+
 public class MyClass {
     
     public static void main(String[] args){
--- JavaAnatomiadeClasses/src/edu/dio/AnatomiaClasses/BoletimEstudantil.java
+++ JavaAnatomiadeClasses/src/edu/dio/AnatomiaClasses/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.dio.AnatomiaClasses;
 public class BoletimEstudantil {
     public static void main(String[] args) {
         //declarando variavel
