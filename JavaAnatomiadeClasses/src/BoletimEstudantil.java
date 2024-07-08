63eb9505a085650a34861f7608d13165c998ccf2
Alison <alisson_gamer2000@outlook.com>
2024-07-08 11:28:26 -0300

    IdentaçãoLogica

---
+++ JavaAnatomiadeClasses/src/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        //declarando variavel
+        int mediaFinal = 6;
+
+        //condição if
+        if(mediaFinal < 6){
+            System.out.println("Reprovado, tenta na próxima!");
+        }else if(mediaFinal == 6){
+            System.out.println("Ficou na média, mas passou!");
+        }else{
+            System.out.println("Aprovado, muito bem!");
+        }
+    }
+}
